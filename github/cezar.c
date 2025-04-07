ca3cfd560387138ab77b64fc8ea38a6c6b018287
Frosttbitten14 <formul8r@gmail.com>
2025-04-08 00:20:16 +0300

    Update cezar.c

--- github/cezar.c
+++ github/cezar.c
@@ -9,16 +9,30 @@ char* cezar_cipher(char* text, int key)
     char* cipher = (char*) malloc(sizeof(char) * (length + 1));
     for(int i = 0; i < length; i++)
     {
+        if(islower(text[i]))
+        {
         cipher[i] = (text[i] - 'a' + key) % 26 + 'a';
+        }
+        else
+        {
+        cipher[i] = (text[i] - 'A' + key) % 26 + 'A';
+        }
     }
     cipher[length] = '\0';
     return cipher;
 }
 
+void write_cipher(char* filename, char* cipher)
+{
+    FILE *fp = fopen(filename, "wb");
+    fprintf(fp, "s", cipher);
+    fclose(fp);
+}
+
 int main()
 {
     char text[MAXN];
-    scanf("%s", text);
+    fgets(text, MAXN, stdin);
     int key;
     scanf("%d", &key);
     char* cipher = cezar_cipher(text, key);
